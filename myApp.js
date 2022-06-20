2e9c7f81036edba5d06a26fc185b0824871a6020
Annika <annika.dahms@gmx.de>
2022-06-20 18:28:57 +0200

    Add REST endpoint for requesting operating sites

--- myApp.js
+++ myApp.js
@@ -17,11 +17,25 @@ rl.on("close", () => {
     console.log(dbOperatingSites);
 });
 
-// write get request for detailes data
+// server requests
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("DB Betriebstellenverzeichnis");
+});
+
+app.get('/:dbSiteCode', (req, res) => {
+    let dbSiteCode = req.params.dbSiteCode;
+    let dbSite = dbOperatingSites.find(a => a[1] === dbSiteCode);
+    if (dbSite) {
+        // build json response with descriptors and values
+        let completeDbSiteInfo = {};
+        for (let i = 0; i < dbOperatingSites[0].length; i++){
+            completeDbSiteInfo[dbOperatingSites[0][i]] = dbSite[i];
+            console.log(completeDbSiteInfo)
+        };
+        res.json(completeDbSiteInfo);
+    }
 });
 
 app.listen(port, () => {
     console.log("App is listening on port 3000")
-})
+});

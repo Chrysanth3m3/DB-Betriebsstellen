f54562e8a80c0e2409d6a68a7f900851bd42eaf6
Annika <annika.dahms@gmx.de>
2022-06-20 17:40:07 +0200

    Read in csv file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const readline = require('readline');
 
+// read csv file
+const stream = fs.createReadStream('./DBNetz-Betriebsstellenverzeichnis-Stand2021-10.csv');
+const rl = readline.createInterface({input: stream});
+let dbOperatingSites = [];
+
+rl.on("line", (row) => {
+    dbOperatingSites.push(row.split(';'));
+});
+
+rl.on("close", () => {
+    console.log(dbOperatingSites);
+});
+
+// write get request for detailes data
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
